be12327969b3bfb86a5789987f9a4f5c92e69b12
teyim <teyimasobo@gmail.com>
2024-02-06 22:18:38 +0100

    modify server.js file

--- app/server.js
+++ app/server.js
@@ -1,21 +1,23 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
+let express = require("express");
+let path = require("path");
+let fs = require("fs");
+let MongoClient = require("mongodb").MongoClient;
+let bodyParser = require("body-parser");
 let app = express();
 
-const DB_USER = process.env.MONGO_DB_USERNAME
-const DB_PASS = process.env.MONGO_DB_PWD
+const DB_USER = process.env.MONGO_DB_USERNAME;
+const DB_PASS = process.env.MONGO_DB_PWD;
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // when starting app locally, use "mongodb://admin:password@localhost:27017" URL instead
 let mongoUrlDockerCompose = `mongodb://${DB_USER}:${DB_PASS}@mongodb`;
@@ -27,27 +29,30 @@ let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 let databaseName = "my-db";
 let collectionName = "my-collection";
 
-app.get('/fetch-data', function (req, res) {
+app.get("/fetch-data", function (req, res) {
   let response = {};
-  MongoClient.connect(mongoUrlDockerCompose, mongoClientOptions, function (err, client) {
-    if (err) throw err;
-
-    let db = client.db(databaseName);
-
-    let myquery = { myid: 1 };
-
-    db.collection(collectionName).findOne(myquery, function (err, result) {
+  MongoClient.connect(
+    mongoUrlDockerCompose,
+    mongoClientOptions,
+    function (err, client) {
       if (err) throw err;
-      response = result;
-      client.close();
 
-      // Send response
-      res.send(response ? response : {});
-    });
-  });
+      let db = client.db(databaseName);
+
+      let myquery = { myId: 1 };
+
+      db.collection(collectionName).findOne(myquery, function (err, result) {
+        if (err) throw err;
+        response = result;
+        client.close();
+
+        // Send response
+        res.send(response ? response : {});
+      });
+    }
+  );
 });
 
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
 });
-
